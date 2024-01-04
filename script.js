6b39eed98a6995dfcc915d4cf9b8a05ee3ce8042
cihouare <issam.chouaref1998@gmail.com>
2024-01-04 20:52:42 +0100

    nabvarr

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("hello word")
+let removeMenu = document.querySelector("body .container nav .nav-list svg")
+let Menu = document.querySelector("body .container nav .nav-list")
+let Menushow = document.querySelector("body .container nav .Menu")
+
+let MenuItem = document.getElementsByClassName("item-list")
+
+
+
+console.log(MenuItem.length)
+removeMenu.addEventListener("click", ()=> {
+    Menu.style.left = "120%"
+})
+
+Menushow.addEventListener("click", ()=> {
+    console.log("Menu clicked")
+    Menu.style.left = "0%"
+})
+
+// document.addEventListener("click", () => {
+//     if()
+// })
